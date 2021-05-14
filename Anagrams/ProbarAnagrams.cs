cbdd9b6947660b196bff211d9ef79a716f561656
Jonathan Diaz <jonaalbert.adm@gmail.com>
2021-05-14 11:05:52 -0400

    Testing

--- Anagrams/ProbarAnagrams.cs
+++ Anagrams/ProbarAnagrams.cs
@@ -8,6 +8,22 @@ namespace Anagrams
 {
     public class ProbarAnagrams
     {
+        public bool AreAnagrams(string s1, string s2)
+        {
+            if (s1 is null || s2 is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            string p1 = new string(s1.OrderBy(x => x).ToArray());
+            string p2 = new string(s2.OrderBy(x => x).ToArray());
+
+            if (p1 == p2)
+            {
+                return true;
+            }
+            return false;
+        }
 
     }
 }
--- AnagramsTest/ProbarAnagramsTest.cs
+++ AnagramsTest/ProbarAnagramsTest.cs
@@ -6,10 +6,38 @@ namespace AnagramsTest
     public class ProbarAnagramsTest
     {
         [Test]
-        public void Test1()
+        public void AreAnagrams_s1_IsNull()
         {
+            ProbarAnagrams probarAnagrams = new ProbarAnagrams();
+            Assert.That(() => probarAnagrams.AreAnagrams(null, "hola"), Throws.ArgumentNullException);
+        }
 
-           
+        [Test]
+        public void AreAnagrams_s2_IsNull()
+        {
+            ProbarAnagrams probarAnagrams = new ProbarAnagrams();
+            Assert.That(() => probarAnagrams.AreAnagrams("hola", null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void AreAnagrams_s1_s2_NotIsNull()
+        {
+            ProbarAnagrams probarAnagrams = new ProbarAnagrams();
+            Assert.That(() => probarAnagrams.AreAnagrams("hey", "hola"), Throws.Nothing);
+        }
+
+        [Test]
+        public void AreAnagrams_IsTrue()
+        {
+            ProbarAnagrams probarAnagrams = new ProbarAnagrams();
+            Assert.That(probarAnagrams.AreAnagrams("romo", "moro"), Is.True);
+        }
+
+        [Test]
+        public void AreAnagrams_IsFalse()
+        {
+            ProbarAnagrams probarAnagrams = new ProbarAnagrams();
+            Assert.That(probarAnagrams.AreAnagrams("hola", "moro"), Is.False);
         }
     }
 }
